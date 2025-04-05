63d054b08e71471d0c5bf93060a4b7f12f13df10
Gustavo Santos <alexandregustavo00@gmail.com>
2025-04-04 21:11:50 -0300

    feat: implementa o modelo de AvalicaoAtividade

---
+++ avaliacao/avaliacao_service/models/avaliacao_atividade.py
@@ -0,0 +1,21 @@
+from django.db import models 
+
+def conteudo_para_avaliacao_path(instance, filename):
+    return f'conteudo_avaliacao/{instance.aluno}/{filename}'
+
+class AvaliacaoAtividade(models.Model):
+    aluno = models.IntegerField()
+    atividade = models.IntegerField()
+    conteudo_para_avaliacao = models.FileField(upload_to=conteudo_para_avaliacao_path)
+    nota = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Avaliação de Atividade: {self.aluno} - {self.atividade} - {self.nota}"
+    
+    class Meta:
+        db_table = 'avaliacao_atividade'
+        verbose_name = 'Avaliação de Atividade'
+        verbose_name_plural = 'Avaliações de Atividades'
+        ordering = ['atividade', 'aluno']
